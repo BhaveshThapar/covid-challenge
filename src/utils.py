2a9f16a545fed77ff39016f309d2654152832d5f
aaditn18 <aaditnilay18@gmail.com>
2026-03-03 13:05:03 -0500

    Fix torch.load weights_only error (PyTorch 2.6 breaking change)
    PyTorch 2.6 changed weights_only default to True, rejecting checkpoints
    that contain numpy scalars in the score field. Add weights_only=False
    since we load only our own trusted checkpoints.

--- src/utils.py
+++ src/utils.py
@@ -172,7 +172,7 @@ class CheckpointManager:
 
     @staticmethod
     def load(path, model, optimizer=None, device="cpu"):
-        ckpt = torch.load(path, map_location=device)
+        ckpt = torch.load(path, map_location=device, weights_only=False)
         model.load_state_dict(ckpt["model_state_dict"])
         if optimizer and "optimizer_state_dict" in ckpt:
             optimizer.load_state_dict(ckpt["optimizer_state_dict"])
